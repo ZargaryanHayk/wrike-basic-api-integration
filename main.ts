0fb97e044f15ed2994d01fbb9e92c06afe19cc4d
Hayk Zargaryan <ZargaryanHayk>
2024-08-29 14:46:42 +0400

    change json data style

--- main.ts
+++ main.ts
@@ -15,27 +15,41 @@ dotenv.config()
 // // ticket_url => 'permalink'
 
 const API_TOKEN = process.env.API_TOKEN as string ;
-const API:string = 'https://www.wrike.com/api/v4/tasks'
 
-class Task { 
-    id!: string 
-    name!: string 
-    assignee!: string 
-    status!: string 
-    collections!: string 
-    created_at!: string 
-    updated_at!: string 
-    ticket_url!: string  
+const API_PROJECT:string = 'https://www.wrike.com/api/v4/folders' 
+
+interface Project {
+
+    project: {
+        ownerIds: string[]
+    },
+    title:string;
+    id: string
 }
-class MyData{
-    id!: string 
-    title!: string 
-    accountId!: string 
-    importance!: string 
-    parentIds!: string 
-    createdDate!: string 
-    updatedDate!: string 
-    permalink!: string
+
+interface Task { 
+
+   
+
+        id: string 
+        name: string 
+        assignee: string 
+        status: string 
+        collections: string 
+        created_at: string 
+        updated_at: string 
+        ticket_url: string  
+    
+}
+interface MyData{
+    id: string 
+    title: string 
+    accountId: string 
+    status: string 
+    parentIds: string 
+    createdDate: string 
+    updatedDate: string 
+    permalink: string
 }
 
 
@@ -43,34 +57,56 @@ const headers = {
     'Authorization': `bearer ${API_TOKEN}`
 }
 
-const config: AxiosRequestConfig = {
-    headers: headers
-};
-
- async function  getWrikeTask() {
-
-   
-    
-    const response =  await axios.get(API, { headers });
+async function getWrikeProjectsId(): Promise<[string, string][]> {
+    const response = await axios.get(API_PROJECT, { headers });
     const data = response.data;
-    console.log(data)
-    const tasks: Task[] = data.data.map((d: MyData )=> ({
-        id: d.id,
-        name: d.title,
-        assignee: d.accountId,
-        status: d.importance,
-        collections: d.parentIds,
-        created_at: d.createdDate,
-        updated_at: d.updatedDate,
-        ticket_url: d.permalink
-    }));  
-    return tasks
+    const idAndTitle: [string, string][] = data.data
+        .filter((element: Project) => element.project && element.title)
+        .map((element: Project) => [element.id, element.title] as [string, string]);
 
+    return idAndTitle;
 }
 
 
+ async function  getWrikeTask(elementArray: [string,string][]) {
 
- function writeJson(data:Task[]){
+        const tasks: { [key: string]: Task[] } = {};
+
+        for(const [id, title] of elementArray){
+
+            const API_TASK:string = `https://www.wrike.com/api/v4/folders/${id}/tasks`
+            const response =  await axios.get(API_TASK, { headers });
+            const data = response.data;
+            tasks[title] = data.data.map((d: MyData) => ({
+                id: d.id,
+                name: d.title,
+                assignee: d.accountId,
+                status: d.status,
+                collections: d.parentIds,
+                created_at: d.createdDate,
+                updated_at: d.updatedDate,
+                ticket_url: d.permalink,
+            }));
+            
+            }; 
+            
+        return tasks
+}
+
+    
+
+    
+
+   
+    
+   
+   
+
+
+
+
+
+ function writeJson(data: { [key: string]: Task[] }){
         
     const fileSaveData = JSON.stringify({...data})
 
@@ -82,7 +118,7 @@ const config: AxiosRequestConfig = {
 }
 
 (async () => {
-    const tasks = await getWrikeTask();
-    console.log(tasks)
-    writeJson(tasks)
+    const elementArray: [string, string][] = await getWrikeProjectsId();
+    const tasks = await getWrikeTask(elementArray);
+    writeJson(tasks);
 })();
